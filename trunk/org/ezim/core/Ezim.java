381c076d146c374e9fd9143f0d74065b183b1473
shinkou <shinkou@1b531cd1-a65f-4aa8-af78-d5d7f6f25e64>
2009-04-16 13:32:51 +0000

    - modified the user manual. - incremented version number.

--- trunk/org/ezim/core/Ezim.java
+++ trunk/org/ezim/core/Ezim.java
@@ -48,7 +48,7 @@ public class Ezim
 	// application name and version
 	public final static String appName = "EZ Intranet Messenger";
 	public final static String appAbbrev = "ezim";
-	public final static String appVer = "1.1.4";
+	public final static String appVer = "1.1.5";
 
 	// thread pool sizes and keep alive time (in minutes)
 	public final static int thPoolSizeCore = 8;
