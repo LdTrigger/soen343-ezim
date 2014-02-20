28b1e799f8003b1c6274af0bfe5c0f331fb47717
shinkou <shinkou@1b531cd1-a65f-4aa8-af78-d5d7f6f25e64>
2014-02-20 04:24:06 +0000

    Fixed minor UI bugs
    - fixed progress bar on incoming file window.
    - fixed progress bar on outgoing file window.
    - modified copyright notices.

--- trunk/org/ezim/ui/EzimFileIn.java
+++ trunk/org/ezim/ui/EzimFileIn.java
@@ -1,7 +1,7 @@
 /*
     EZ Intranet Messenger
 
-    Copyright (C) 2007 - 2013  Chun-Kwong Wong
+    Copyright (C) 2007 - 2014  Chun-Kwong Wong
     chunkwong.wong@gmail.com
     http://ezim.sourceforge.net/
 
@@ -685,7 +685,7 @@ public class EzimFileIn
 	 */
 	public void setProgressed(long lIn)
 	{
-		this.jpbProgress.setValue((int) (lIn / (this.filesize / 100)));
+		this.jpbProgress.setValue((int) ((lIn * 100) / this.filesize));
 	}
 
 	/**
