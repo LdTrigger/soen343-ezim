0c05c6998f12e1af7353f0a910a8f03de0e13a9a
Abhi728 <abhi564@yahoo.ca>
2014-11-29 13:29:53 -0500

    Ezim New Class "EzimSocketBinder" Implements Runnable . New Functions/Variables Added
    The new "EzimSocketBinder" class now implements the Runnable interface.
    
    A new function "EzimFileType" has been added that will take a string as
    a parameter which will define whether it is a file request , confirm ,
    response or file sending.
    
    The variables associated with the functions has been added.
    
    A new function "doRun" has been created that will be called from the 4
    other classes. It takes a unique string as a parameter that will depend
    on which class is calling it.
    
    The "run" function for this class has been created.

--- org/ezim/core/EzimSocketBinder.java
+++ org/ezim/core/EzimSocketBinder.java
@@ -34,7 +34,7 @@ import org.ezim.ui.EzimFileOut;
 import org.ezim.core.EzimFrxList;
 import org.ezim.core.EzimFtxList;
 
-public class EzimSocketBinder
+public class EzimSocketBinder implements Runnable
 {
 	protected InetAddress addr = null;
 	protected int port = -1;
@@ -43,4 +43,44 @@ public class EzimSocketBinder
 	
 	protected boolean blnRes = false;
 	protected boolean blnConfirm = false;
+	
+	private boolean confirm = false;
+	private boolean respond = false;
+	private boolean send = false;
+	private boolean request = false;
+	
+	// Will determine the fileType and run the appropriate code (to be implemented in run)
+	
+	public void EzimFileType(String str)
+	{
+		if(str.equals("respond"))
+		{
+			this.respond = true;
+		}
+		else if(str.equals("request"))
+		{
+			this.request = true;
+		}
+		else if(str.equals("confirm"))
+		{
+			this.confirm = true;
+		}
+		else if(str.equals("send"))
+		{
+			this.send = true;
+		}
+	}
+	
+	// This function will be called by one of the classes(Requester , Responder etc...)
+	
+	public void doRun(String fileType)
+	{
+		EzimFileType(fileType);
+		run();
+	}
+	
+	public void run()
+	{
+		// Function to be implemented in coming patchsets
+	}
 }
